caa6f7dab6c77e52e03577d11f6fdf0d664d2bed
pranto322 <parijatsaha435@gmail.com>
2023-09-11 09:46:21 +0600

    git commit -m "modified"

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -42,9 +42,9 @@ export const ProductDetail = () => {
         <h1 className="mt-10 mb-5 text-4xl text-center font-bold text-gray-900 dark:text-slate-200">{product.name}</h1>
         <p className="mb-5 text-lg text-center text-gray-900 dark:text-slate-200">{product.overview}</p>
         <div className="flex flex-wrap justify-around">
-          <div className="max-w-xl my-3">
-            <img className='rounded' src={product.image_local} alt={product.name} />
-          </div>
+        <div className='max-w-md my-3'>
+             <img className='rounded' src={product.image_local} alt={product.name} /> 
+            </div>
           <div className="max-w-xl my-3">
             <p className="text-3xl font-bold text-gray-900 dark:text-slate-200">
               <span className="mr-1">$</span>
@@ -59,7 +59,7 @@ export const ProductDetail = () => {
               {product.best_seller && <span className="font-semibold text-amber-500 border bg-amber-50 rounded-lg px-3 py-1 mr-2">BEST SELLER</span>}
               {product.in_stock && <span className="font-semibold text-emerald-600	border bg-slate-100 rounded-lg px-3 py-1 mr-2">INSTOCK</span>}
               {!product.in_stock && <span className="font-semibold text-rose-700 border bg-slate-100 rounded-lg px-3 py-1 mr-2">OUT OF STOCK</span>}
-              <span className="font-semibold text-blue-500 border bg-slate-100 rounded-lg px-3 py-1 mr-2">{product.size} MB</span>
+              
             </p>
             <p className="my-3">
               {!inCart && <button onClick={() => addToCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${product.in_stock ? "" : "cursor-not-allowed"}`} disabled={product.in_stock ? "" : "disabled"}>Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button>}
